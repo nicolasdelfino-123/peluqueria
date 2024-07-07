6f2c0a7911b38769ee334e447a95b1644c24e00a
Nicolas <nicolasdelfino585@gmail.com>
2024-07-07 02:52:22 -0300

    modificada letra de negra a blanca en formulario, ok. tratando de solucionar bug al confirmar reserva

--- app_horarios/forms.py
+++ app_horarios/forms.py
@@ -10,5 +10,10 @@ class ReservaForm(forms.ModelForm):
     class Meta:
         model = Turno
         fields = ['nombre_cliente', 'apellido_cliente', 'telefono_cliente']
+        labels = {
+            'nombre_cliente': 'Nombre',
+            'apellido_cliente': 'Apellido',
+            'telefono_cliente': 'Teléfono'
+        }
 
 
